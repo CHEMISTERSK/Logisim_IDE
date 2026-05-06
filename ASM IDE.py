f7d30ed38533b08b5bc1ee0cfb1319cb4c0caa22
CHEMISTERSK <vavrotom10@gmail.com>
2026-05-06 23:12:43 +0200

    extending IDE GUI

--- ASM IDE.py
+++ ASM IDE.py
@@ -1,20 +1,39 @@
+import json
 from customtkinter import *
 from pathlib import Path
 from PIL import Image
 
 from virtual_cpu_compiler import compiler
 
-root = CTk()
+root  =  CTk()
 root.geometry("1920x1080")
 root.title("Virtual CPU IDE")
 root.after(25, lambda: root.state("zoomed"))
 
-icon_path = Path(__file__).with_name("icons") / "compile_icon.png"
 
-icon = CTkImage(light_image = Image.open(icon_path),
-                dark_image = Image.open(icon_path),
-                size = (25, 25),
-                )
+def scale(root):
+    dpi_scale  =  root.winfo_fpixels('1i') / 72
+
+    set_window_scaling(dpi_scale)
+    set_widget_scaling(dpi_scale)
+
+def create_file() -> None:
+    with open(f"{os.path.dirname(os.path.abspath(__file__))}\\projects\\newfile.txt", "w"):
+            pass
+
+compile_icon_path = Path(__file__).with_name("icons") / "compile_icon.png"
+folder_icon_path = Path(__file__).with_name("icons") / "folder.png"
+new_file_icon_path = Path(__file__).with_name("icons") / "new_file.png"
+
+compile_icon = CTkImage(light_image = Image.open(compile_icon_path),
+                dark_image = Image.open(compile_icon_path),
+                size = (25, 25))
+folder_icon = CTkImage(light_image = Image.open(folder_icon_path),
+                       dark_image = Image.open(folder_icon_path),
+                       size = (25, 25))
+new_file_icon = CTkImage(light_image = Image.open(new_file_icon_path),
+                       dark_image = Image.open(new_file_icon_path),
+                       size = (25, 25))
 
 
 terminal_frame = CTkFrame(root,
@@ -26,18 +45,20 @@ terminal_frame = CTkFrame(root,
 
 terminal_cards = CTkFrame(terminal_frame,
                           width = 1120,
-                          height = 25
+                          height = 30
                           )
 
 terminal = CTkTextbox(terminal_frame,
                       width = 1120,
                       height = 150,
-                      state = "disabled"
+                      state = "disabled",
+                      font = ("Consolas", 12)
                       )
 
 terminal_input = CTkEntry(terminal_frame,
                           width = 1120,
-                          height = 30)
+                          height = 30
+                          )
 
 
 def compilation():
@@ -58,7 +79,7 @@ hot_bar = CTkFrame(work_space,
                    height = 30)
 
 compile_button = CTkButton(work_space,
-                           image = icon,
+                           image = compile_icon,
                            text = "",
                            width = 25,
                            height = 25,
@@ -66,40 +87,66 @@ compile_button = CTkButton(work_space,
 
 editor = CTkTextbox(work_space,
                     width = 1120,
-                    height = 500
+                    height = 495
                     )
 
 
 file_explorer = CTkFrame(root,
                          width = 384,
                          height = 785,
-                         border_width = 2.5,
-                         border_color = "white" 
+                         border_width  =  2.5,
+                         border_color  =  "white" 
                          )
 
+path_link = CTkButton(file_explorer,
+                      text = "Open Projects Folder ",
+                      font = ("arial", 15, "bold"),
+                      width = 120,
+                      height = 30,
+                      image = folder_icon,
+                      command = lambda: os.startfile(f"{os.path.dirname(os.path.abspath(__file__))}\\projects")
+                      )
+
+new_file = CTkButton(file_explorer,
+                     text = "",
+                     width = 30,
+                     height = 30,
+                     image = new_file_icon,
+                     command = lambda: create_file()
+                     )
 
 
+root.grid_columnconfigure(0, weight = 1)
+root.grid_columnconfigure(1, weight = 4)
+
+root.grid_rowconfigure(0, weight = 4)
+root.grid_rowconfigure(1, weight = 1)
 
 
+file_explorer.grid(row = 0, column = 0, rowspan = 2, sticky = "nsew", padx = 10, pady = 10)
 
-work_space.place(x = 395, y = 5)
-work_space.grid_propagate(False)
-
-hot_bar.place(x = 7, y = 7)
-compile_button.place(x = 1085, y = 5)
-editor.place(x = 7, y = 40)
+path_link.grid(row = 0, column = 0, sticky = "nw", padx = (10, 5), pady = 10)
+new_file.grid(row = 0, column = 1, sticky = "nw", padx = 5, pady = 10)
 
 
-file_explorer.place(x = 5, y = 5)
-file_explorer.grid_propagate(False)
+work_space.grid(row = 0, column = 1, sticky = "nsew", padx =  10, pady = 10)
+
+work_space.grid_columnconfigure(0, weight = 1)
+work_space.grid_columnconfigure(1, weight = 0)
+work_space.grid_rowconfigure(1, weight = 1)
+
+hot_bar.grid(row = 0, column = 0, sticky = "ew", padx =  (10, 2.5), pady = (10, 5))
+compile_button.grid(row = 0, column = 1, sticky = "e", padx = (2.5, 10), pady = (10, 5))
+editor.grid(row = 1, column = 0, columnspan = 2, sticky = "nsew", padx = 10, pady = (5, 10))
 
 
-terminal_frame.place(x = 395, y = 560)
-terminal_frame.grid_propagate(False)
+terminal_frame.grid(row = 1, column = 1, sticky = "nsew", padx =  10, pady = 10)
 
-terminal_cards.place(x = 7, y = 7)
-terminal.place(x = 7, y = 35)
-terminal_input.place(x = 7, y = 190)
+terminal_frame.grid_columnconfigure(0, weight = 1)
+terminal_frame.grid_rowconfigure(1, weight = 1)
 
+terminal_cards.grid(row = 0, column = 0, sticky = "ew", padx = 10, pady = (10, 2.5))
+terminal.grid(row = 1, column = 0, sticky = "nsew", padx = 10, pady = 5)
+terminal_input.grid(row = 2, column = 0, sticky = "ew", padx = 10, pady = (2.5, 10))
 
 root.mainloop()
